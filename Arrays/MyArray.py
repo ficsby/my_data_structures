d374b1208a189bb541bbb2d96c97e0c78b14d947
Francis Buendia <francisgbuendia@gmail.com>
2018-11-09 20:26:54 -0800

    initial project version

---
+++ Arrays/MyArray.py
@@ -0,0 +1,41 @@
+import ctypes
+class MyArray(object):
+
+    def __init__(self):
+        # initial state of my array has 0 elements (n) and capacity of 1 (how many elements it is able to hold before it needs to resize)
+        self._n = 0
+        self._capacity = 1
+        # Creates a null ptr array obj
+        self._array = (self.capacity * ctypes.py_object)()
+        #self.array[:] = [None] * self.size
+    
+    """
+        String representation of the array. Normally if you print(object) or in this case object is my array, the memory address of the array
+        is printed. This string representation allows me to print the actual contents of the array when a call to "print(object)" is made.
+    """
+    def __str__(self):
+        string = ""
+        string += "["
+        for i, val in enumerate(self.array):
+            if i + 1 < len(self.array):
+                string += "'{}',".format(str(val))
+            else:
+                string += "'{}'".format(str(val))
+        string += "]"
+        return string
+    
+    def append(self, item):
+        pass
+
+    def insert(self, index, item):
+        pass
+
+    def remove(self, item):
+        pass
+    
+    def size(self):
+        return self.size
+    
+    def contains(self, item):
+        pass
+    
---
+++ Arrays/TestArray.py
@@ -0,0 +1,7 @@
+from MyArray import MyArray
+class TestArray(object):
+    if __name__== "__main__":
+        array = MyArray(2)
+        x = array.size
+        print(x)
+        print(array)
